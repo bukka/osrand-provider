1e398853ec13e7950fe1b41b0fd28ac37f098329
Jakub Zelenka <jakub.openssl@gmail.com>
2025-01-23 15:06:02 +0100

    Make example work with system installation

--- examples/rand-gen.sh
+++ examples/rand-gen.sh
@@ -6,17 +6,26 @@ osr_example_dir=$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ -z "$OPENSSL_DIR" ]; then
     OPENSSL_DIR=/usr/local/ssl
-    echo "Sertting openssl dir to $OPENSSL_DIR"
+    echo "Setting openssl dir to $OPENSSL_DIR"
 fi
-if [ ! -d "$OPENSSL_DIR" ]; then
-    echo "OPENSSL_DIR does not exist"
+if [ -d "$OPENSSL_DIR" ]; then
+    OPENSSL_MODULES_DIR="$OPENSSL_DIR/lib64/ossl-modules"
+    OPENSSL_BIN=$OPENSSL_DIR/bin/openssl
+elif [ -d "/usr/lib/x86_64-linux-gnu/ossl-modules" ]; then
+    OPENSSL_MODULES_DIR="/usr/lib/x86_64-linux-gnu/ossl-modules"
+    OPENSSL_BIN=/usr/bin/openssl
+elif [ -d "/usr/lib/ossl-modules" ]; then
+    OPENSSL_MODULES_DIR="/usr/lib/ossl-modules"
+    OPENSSL_BIN=/usr/bin/openssl
+else
+    echo "OPENSSL_DIR or OpenSSL system dir do not exist"
     exit 1
 fi
 
 export OPENSSL_CONF="$osr_example_dir/openssl.cnf"
 export OPENSSL_CONF_IN="$OPENSSL_CONF.in"
 
-sed "s|__OPENSSL_DIR__|$OPENSSL_DIR/lib64/ossl-modules|g" $OPENSSL_CONF_IN > $OPENSSL_CONF
+sed "s|__OPENSSL_MODULES_DIR__|$OPENSSL_MODULES_DIR|g" $OPENSSL_CONF_IN > $OPENSSL_CONF
 
 echo "Using OPENSSL_CONF=$OPENSSL_CONF"
 
@@ -28,5 +37,5 @@ rm -f $osr_log_file
 echo "Using OSRAND_PROVIDER_DEBUG=file:$osr_log_file,level:6"
 export OSRAND_PROVIDER_DEBUG="file:$osr_log_file,level:6"
 
-echo $OPENSSL_DIR/bin/openssl rand -hex 16
-$OPENSSL_DIR/bin/openssl rand -hex 16
+echo $OPENSSL_BIN rand -hex 16
+$OPENSSL_BIN rand -hex 16
