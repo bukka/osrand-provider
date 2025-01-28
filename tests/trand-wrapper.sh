1d420cd30f3ba5283492f4104091c51ed482919a
Jakub Zelenka <jakub.openssl@gmail.com>
2025-01-28 19:31:21 +0100

    Add initial test structure

---
+++ tests/trand-wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+# Copyright (C) 2025 Jakub Zelenka <jakub.openssl@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+# TODO: write the actual test
---
+++ tests/trand.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <openssl/rand.h>
+
+int main() {
+    unsigned char buffer[16];
+
+    // Generate random bytes (first call)
+    if (RAND_bytes(buffer, sizeof(buffer)) != 1) {
+        fprintf(stderr, "Error: RAND_bytes failed on the first call.\n");
+        return 1;
+    }
+
+    printf("Random bytes (call 1): ");
+    for (size_t i = 0; i < sizeof(buffer); i++) {
+        printf("%02x", buffer[i]);
+    }
+    printf("\n");
+
+    // Generate random bytes (second call)
+    if (RAND_bytes(buffer, sizeof(buffer)) != 1) {
+        fprintf(stderr, "Error: RAND_bytes failed on the second call.\n");
+        return 1;
+    }
+
+    printf("Random bytes (call 2): ");
+    for (size_t i = 0; i < sizeof(buffer); i++) {
+        printf("%02x", buffer[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
